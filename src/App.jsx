ac77bbb4813b8bb177a051056d04da9e5eb631e4
Kaya Woods <kayanevada@gmail.com>
2025-03-10 15:15:25 -0700

    imported use State

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,29 @@ import NavBar from "./components/NavBar/NavBar";
 import MailboxList from "./components/MailboxList/MailboxList";
 import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
 import MailboxForm from "./components/MailboxForm/MailboxForm";
+import { Route, Routes } from 'react-router';
 
 
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([])
+  
+  return (
+      <>
+         <NavBar />
+         <h1>Mailboxes!</h1>
+        <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="/mailboxes" element={<MailboxList />} />
+        <Route path="/new-mailbox" element={<MailboxForm />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />}
+          />
+        <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
+        </Routes>
+      </>
+    );
+
+  
 };
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,23 @@
+import { link } from "react-router"
 
-const NavBar  = () => {
-    
-}
+const NavBar = () => {
+    return (
+      <nav>
+       <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/mailboxes'>mailboxes</Link>
+        </li>
+        <li>
+          <Link to="/mailboxes/new-mailbox">New Mailbox</Link>
+        </li>
+      </ul>
+      </nav>
+    );
+  };
+  
 
 
 export default NavBar;
