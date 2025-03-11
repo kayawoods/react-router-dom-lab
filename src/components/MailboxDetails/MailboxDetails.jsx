fd3fe34dbdd96538efe51ccd2ce58d1f78b0c1e9
Kaya Woods <kayanevada@gmail.com>
2025-03-11 09:46:28 -0700

    dropdown function

--- src/components/MailboxDetails/MailboxDetails.jsx
+++ src/components/MailboxDetails/MailboxDetails.jsx
@@ -1,29 +1,26 @@
-
-import { useParams } from "react-router";
+import { useParams } from 'react-router';
 
 const MailboxDetails = (props) => {
-   
     const { mailboxId } = useParams();
-const selectedBox = props.mailboxes.find((mailbox) => (
-   mailbox._id === Number(mailboxId)
-));
 
-return (
-    <>
-      <h2>Box Details</h2>
-      <dl className="left-align">
-        <dt>Box Holder:</dt>
-        <dd>{ selectedBox.boxOwner } </dd>
-        <dt>Box size:</dt>
-        <dd>{ selectedBox.boxSize }</dd>
-      </dl>
-    </>
-  );
+    const selectedBox = props.mailboxes.find((mailbox) => (
+        mailbox._id === Number(mailboxId)
+    ));
+
+    return (
+        <>
+            <h2> Mailbox {selectedBox._id}</h2>
+            <dl>
+                <h3>Details</h3>
+                
+                <dl>Boxholder: {selectedBox.boxOwner}</dl>
+                
+                <dl>Box Size: {selectedBox.boxSize}</dl>
+               
+            </dl>
+        </>
+    );
 };
 
 
-
-
-
-
-export default MailboxDetails 
+export default MailboxDetails;
--- src/components/MailboxForm/MailboxForm.jsx
+++ src/components/MailboxForm/MailboxForm.jsx
@@ -2,57 +2,53 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 const initialState = {
-    mailbox: '',
-    boxSize: '',
-    boxOwner: '',
+  mailbox: '',
+  boxSize: '',
+  boxOwner: '',
 }
 
 const MailboxForm = (props) => {
-    const [formData, setFormData] = useState(initialState);
-    const navigate = useNavigate();
+  const [formData, setFormData] = useState(initialState);
+  const navigate = useNavigate();
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        props.addBox(formData);
-        setFormData(initialState);
-        navigate('/mailboxes');
-      };
-    
-      const handleChange = ({ target }) => {
-        setFormData({ ...formData, [target.name]: target.value });
-      };
-      return (
-        <main>
-          <h2>New Mailbox</h2>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="mailbox">Mailbox:</label>
-            <input
-              type="text"
-              id="mailbox"
-              name="mailbox"
-              value={formData.name}
-              onChange={handleChange}
-            />
-            <label htmlFor="boxSize">Box Size:</label>
-            <input
-              type="text"
-              id="boxSize"
-              name="boxSize"
-              value={formData.boxSize}
-              onChange={handleChange}
-            />
-            <label htmlFor="boxOwner">Box Owner:</label>
-            <input
-              type="text"
-              id="boxOwner"
-              name="boxOwner"
-              value={formData.boxOwner}
-              onChange={handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-        </main>
-      );
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.addBox(formData);
+    setFormData(initialState);
+    navigate('/mailboxes');
+  };
+
+  const handleChange = ({ target }) => {
+    setFormData({ ...formData, [target.name]: target.value });
+  };
+  return (
+    <main>
+      <h2>New Mailbox</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="boxOwner">Enter a Boxholder:</label>
+        <input
+          type="text"
+          id="boxOwner"
+          name="boxOwner"
+          value={formData.boxOwner}
+          onChange={handleChange}
+        />
+        <label htmlFor="boxSize">Box Size:</label>
+        <select
+          id="boxSize"
+          name="boxSize"
+          value={formData.boxSize}
+          onChange={handleChange}
+        >
+          <option value="">Select a size</option>
+          <option value="small">small</option>
+          <option value="medium">medium</option>
+          <option value="large">large</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 
 
 };
